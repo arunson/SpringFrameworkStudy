828e60d96f78fc16f819111bfe8b31fc007831e0
arunson <jh.kkwon@gmail.com>
2014-08-23 18:26:57 +0900

    week2

---
+++ 1-5/src/sample1/MessageBeanImpl.java
@@ -0,0 +1,20 @@
+package sample1;
+
+public class MessageBeanImpl implements MessageBean{
+	
+	private String name;
+	
+	public void setName(String name){
+		this.name=name;
+	}
+	
+	@Override
+	public void sayHello() {
+		// TODO Auto-generated method stub
+		try{
+			Thread.sleep(1000);
+		} catch(InterruptedException e){}
+		System.out.println("Hello, "+name+"!");
+	}
+	
+}
---
+++ 2-1/src/ui/MenuUi.java
@@ -0,0 +1,50 @@
+package ui;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MenuUi extends AbstractUiTemplate {
+	private SelectTeamUi selectTeamUi;
+	
+	public void setSelectTeamUi(SelectTeamUi selectTeamUi){
+		this.selectTeamUi = selectTeamUi;
+	}
+	
+	protected void showMenu(){
+		System.out.println(".....................");
+		System.out.println("-선수명단- -메뉴-");
+		System.out.println("");
+		System.out.println("1.종료");
+		System.out.println("2.팀 목록");
+		System.out.println("");
+		System.out.println("번호를 입력한 후 Enter키를 눌러주세요.");
+		
+	}
+	
+	protected int getMaxMenuNumber(){
+		return 2;
+	}
+	
+	protected int getMinMenuNumber(){
+		return 1;
+	}
+	
+	protected void execute(int number){
+		switch (number){
+		case 1:
+			System.out.println("종료되었습니다.");
+			System.exit(0);
+		case 2:
+			this.selectTeamUi.show();
+			break;
+		}
+	}
+	
+	public static void main(String[] args){
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
+		MenuUi menuUi=context.getBean(MenuUi.class);
+		while (true){
+			menuUi.show();
+		}
+	}
+}
